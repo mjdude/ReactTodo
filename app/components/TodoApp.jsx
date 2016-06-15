0404de13cf54fc0e82f3883507bc0b802d6760b3
mjdude <mo@syncspot.net>
2016-06-15 16:17:32 -0700

    implemented list pattern for todos

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,34 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: [
+        {
+          id: 1 ,
+          text: 'Walk the dog',
+        },
+        {
+          id: 2 ,
+          text: 'Clean the yard',
+        },
+        {
+          id: 3 ,
+          text: 'Complete React Tutorial',
+        },
+        {
+          id: 4 ,
+          text: 'Call gf',
+        }
+      ],
+    }
+  },
   render: function(){
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}></TodoList>
       </div>
     )
   },
