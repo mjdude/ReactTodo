899950de331c4dc69500ef78ddb27d7615772770
mjdude <mo@syncspot.net>
2016-08-05 17:25:39 -0700

    partial login page style

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 export var Login = React.createClass({
       render() {
-        return <p>Login component rendering</p>
+        return (
+          <div>
+            <h1 className="page-title">Login Page</h1>
+            <div className="row">
+              <div className="colum small-centered small-10 medium-6 large-4">
+                <div className="callout callout-auth">
+                  <h3>Login</h3>
+                  <p>
+                    Login with GitHub account below
+                  </p>
+                  <button className="button">Login With Github</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
       }
   });
 
