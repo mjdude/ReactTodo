1bc1870077bc2fe3b6a61264046fe36cc33de3cb
mjdude <mo@syncspot.net>
2016-08-11 15:22:05 -0700

    wipte todos on logout

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -96,6 +96,25 @@ describe('Reducers', () => {
             expect(res[0]).toEqual(todos[0]);
 
         });
+
+        it('should empty todos on logout', () => {
+          var todos = [
+              {
+                  id: '111',
+                  text: 'anything',
+                  completed: false,
+                  completedAt: undefined,
+                  createdAt: 33000
+              }
+          ];
+          var action = {
+            type: 'LOGOUT'
+          };
+
+          var res = reducers.todosReducer(df(todos), df(action));
+
+          expect(res).toEqual([]);
+        });
     });
 
     describe('authReducer', () => {
